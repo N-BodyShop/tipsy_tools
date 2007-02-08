7baf05615288ce5508f8afe898ecaa8f679aeeab
stinson <stinson>
2007-02-08 01:50:41 +0000

    Changed to reflect changes to tipsydefs.h

--- ascii2std.c
+++ ascii2std.c
@@ -8,118 +8,7 @@
 #include <malloc.h>
 #include "tipsydefs.h"
 #include <rpc/types.h>
-#include <rpc/xdr.h>
 
-static XDR xdrs;
-
-int xdr_header()
-{
-  int pad = 0;
-  
-  if(xdr_double(&xdrs, &header.time) != TRUE)
-    return 0;
-  if(xdr_int(&xdrs, &header.nbodies) != TRUE)
-    return 0;
-  if(xdr_int(&xdrs, &header.ndim) != TRUE)
-    return 0;
-  if(xdr_int(&xdrs, &header.nsph) != TRUE)
-    return 0;
-  if(xdr_int(&xdrs, &header.ndark) != TRUE)
-    return 0;
-  if(xdr_int(&xdrs, &header.nstar) != TRUE)
-    return 0;
-  if(xdr_int(&xdrs, &pad) != TRUE)
-    return 0;
-  return 1;
-}
-
-int xdr_gas(struct gas_particle *gas)
-{
-  if(sizeof(Real) == sizeof(float))
-    {
-      if(xdr_float(&xdrs, &gas->mass) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->pos[0]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->pos[1]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->pos[2]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->vel[0]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->vel[1]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->vel[2]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->rho) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->temp) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->hsmooth) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->metals) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &gas->phi) != TRUE)
-	return 0;
-      return 1;
-    }
-}  
-
-int xdr_dark(struct dark_particle *dark)
-{
-  if(sizeof(Real) == sizeof(float))
-    {
-      if(xdr_float(&xdrs, &dark->mass) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->pos[0]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->pos[1]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->pos[2]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->vel[0]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->vel[1]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->vel[2]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->eps) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &dark->phi) != TRUE)
-	return 0;
-      return 1;
-    }
-}  
-
-int xdr_star(struct star_particle *star)
-{
-  if(sizeof(Real) == sizeof(float))
-    {
-      if(xdr_float(&xdrs, &star->mass) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->pos[0]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->pos[1]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->pos[2]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->vel[0]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->vel[1]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->vel[2]) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->metals) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->tform) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->eps) != TRUE)
-	return 0;
-      if(xdr_float(&xdrs, &star->phi) != TRUE)
-	return 0;
-      return 1;
-    }
-}  
 
 int
 main()
@@ -131,9 +20,11 @@ main()
     int nstar ;
     int count ;
     int i;
-    struct gas_particle *gp, *lastgp;
-    struct dark_particle *dp, *lastdp ;
-    struct star_particle *sp, *lastsp ;
+    struct gas_particle *gp, *lastgp, *gas_particles;
+    struct dark_particle *dp, *lastdp, *dark_particles;
+    struct star_particle *sp, *lastsp, *star_particles;
+    struct dump header;
+    XDR xdrs;
 
     xdrstdio_create(&xdrs, stdout, XDR_ENCODE);
 
@@ -314,15 +205,15 @@ main()
 	/*
 	 * Now write it out
 	 */
-	xdr_header();
+	xdr_header(&xdrs, &header);
 	for(i = 0; i < header.nsph; i++) {
-	    xdr_gas(&gas_particles[i]);
+	    xdr_gas(&xdrs,&gas_particles[i]);
 	}
 	for(i = 0; i < header.ndark; i++) {
-	    xdr_dark(&dark_particles[i]);
+	    xdr_dark(&xdrs,&dark_particles[i]);
 	}
 	for(i = 0; i < header.nstar; i++) {
-	    xdr_star(&star_particles[i]);
+	    xdr_star(&xdrs,&star_particles[i]);
 	}
 	fprintf(stderr, "read time %f\n",header.time) ;
     }
